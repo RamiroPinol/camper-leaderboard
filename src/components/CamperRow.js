7cf43cf6c934b6ac7436d1a0e782aa999890fe94
RamiroPinol <ramirop@protonmail.com>
2017-04-22 20:11:47 -0300

    added link to profile on camper name

--- src/components/CamperRow.js
+++ src/components/CamperRow.js
@@ -6,7 +6,10 @@ function CamperRow(props) {
       <td>{props.index + 1}</td>
       <td className="avatarCell">
         <img src={props.camper.img} alt="avatar"/>
-        <p><b>{props.camper.username}</b></p>
+        <p><b><a
+          href={`https://www.freecodecamp.com/${props.camper.username}`}>
+          {props.camper.username}
+        </a></b></p>
       </td>
       <td>{props.camper.recent}</td>
       <td>{props.camper.alltime}</td>
