d54b93dc8a245ad606fecedcfbb8fa2635bb2832
RamiroPinol <ramirop@protonmail.com>
2017-04-22 20:38:17 -0300

    added my profile and repo info

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
       <div className="App">
         <header>
           <h2>RP FCC Camper Leaderboard</h2>
+          <p>Coded by <a href="https://ramiropinol.github.io/">Ramiro Piñol</a>. Repo on <a href="https://github.com/RamiroPinol/camper-leaderboard">Github</a></p>
         </header>
         <CampersList />
       </div>
